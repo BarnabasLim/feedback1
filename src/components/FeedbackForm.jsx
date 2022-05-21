046cf611626a0f693eb0144ddcec4f00ca577068
BarnabasLim <blim048@e.ntu.edu.sg>
2022-05-21 22:56:19 +0800

    Text input check

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -3,8 +3,27 @@ import Card from './shared/Card'
 import Button from './shared/Button'
 const FeedbackForm = () => {
   const [text, setText]=useState('')
+  const [btnDisabled,setBtnDisabled]=useState(true)
+  const [message, setMessage]=useState('')
+
+
+
   const handleTextChange=(e)=>{
-      setText(e.target.Value);
+      const temp=e.target.value
+      console.log(temp +'length'+temp.trim().length)
+      if(temp===''){
+        setMessage(null)
+        setBtnDisabled(true)
+      }else if(temp!=='' && temp.trim().length<=10){
+        setMessage('text must be at least 10 character')
+        setBtnDisabled(true)
+      }else{
+        setMessage(null)
+        setBtnDisabled(false)
+      }
+      
+      setText(e.target.value);
+      
   }
 
   return (
@@ -13,8 +32,9 @@ const FeedbackForm = () => {
             <h2>how would you rate your service with us?</h2>
             <div className="input-group">
                 <input onChange={handleTextChange} type="text" placeholder='Write a review' value={text} />
-                <Button type={'submit'} version='primary'>send</Button>
+                <Button type={'submit'} version='primary' isDisabled={btnDisabled}>send</Button>
             </div>
+            {message && <div className='message'>{message}</div>}
         </form>
     </Card>
   )
