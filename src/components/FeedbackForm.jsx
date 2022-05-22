6fa76b849039694f76ffd6f6042e65f933d9f8f2
BarnabasLim <blim048@e.ntu.edu.sg>
2022-05-22 19:11:19 +0800

    Rating Structure

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,8 +1,10 @@
 import React,{useState} from 'react'
 import Card from './shared/Card'
 import Button from './shared/Button'
+import RatingSelect from './RatingSelect'
 const FeedbackForm = () => {
   const [text, setText]=useState('')
+  const [rating,setRating]=useState(10)
   const [btnDisabled,setBtnDisabled]=useState(true)
   const [message, setMessage]=useState('')
 
@@ -28,7 +30,9 @@ const FeedbackForm = () => {
   return (
     <Card>
         <form>
+            
             <h2>how would you rate your service with us?</h2>
+            <RatingSelect rating={rating} setRating={setRating}/>
             <div className="input-group">
                 <input onChange={handleTextChange} type="text" placeholder='Write a review' value={text} />
                 <Button type={'submit'} version='primary' isDisabled={btnDisabled}>send</Button>
---
+++ src/components/RatingSelect.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+const RatingSelect = ({rating,setRating}) => {
+  const handleChange=(e)=>{
+    console.log("print"+e.currentTarget.value)
+    setRating(+e.currentTarget.value)
+  }
+  return (
+    <ul className='rating'>
+        {Array.from(Array(10).keys()).map(
+        (item)=>{
+            item=item+1;
+            return (
+                <li key={item}>
+                    <input 
+                        type="radio"
+                        id={`num${item}`}
+                        name='rating'
+                        value={`${item}`}
+                        onChange={handleChange}
+                        checked={rating===item}
+                    />
+                    <label htmlFor={`num${item}`}>{item}</label>
+                </li>
+            )   
+        }
+        )}
+    </ul>
+  )
+}
+
+export default RatingSelect
