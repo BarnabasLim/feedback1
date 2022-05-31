efe123828c737c004b8639463130f05bb7d6fc35
BarnabasLim <blim048@e.ntu.edu.sg>
2022-05-31 13:44:46 +0800

    Remove Unused code

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
-import React,{useState} from 'react'
+import React from 'react'
 import { BrowserRouter as Router, Route ,Routes } from 'react-router-dom'
 import './App.css'
-import {v4 as uuidV4} from 'uuid'
 //UseContext
-import FeedbackContext from './context/FeedbackContext'
 import { FeedbackProvider } from './context/FeedbackContext'
 
 import Header from './components/Header'
 import FeedbackStats from './components/FeedbackStats'
 import FeedbackList from './components/FeedbackList'
-import { FeedbackData } from './data/FeedbackData'
 import FeedbackForm from './components/FeedbackForm'
 import AboutIconLink from './components/AboutIconLink'
 
@@ -18,22 +15,6 @@ import AboutPage from './pages/AboutPage'
 
 function App() {
   //Feedback Entity
-  const [Feedback,setFeedback]=useState(FeedbackData)
-  //Feedback Methods
-  const deleteFeedback=(id)=>{
-    const rmvItem = document.getElementById(id);
-    rmvItem.classList.add('disappear')
-    setTimeout(()=>{setFeedback((prev)=>{return prev.filter((item)=>item.id!==id)})}, 200)
-    // console.log(id)
-    // setFeedback((prev)=>{return prev.filter((item)=>item.id!==id)})
-
-  }
-
-  const addFeedback=(newFeedback)=>{
-    newFeedback.id=uuidV4()
-    console.log(newFeedback)
-    setFeedback((prev)=>{return [newFeedback,...Feedback]})
-  }
   return (
     <>
     <FeedbackProvider>
