a5dbe3d74fbff07763d87fd17896f4eead3c8cae
BarnabasLim <blim048@e.ntu.edu.sg>
2022-06-22 15:04:59 +0800

    Spinner Styles

--- src/components/shared/Spinner.jsx
+++ src/components/shared/Spinner.jsx
@@ -3,7 +3,7 @@ import spinner from '../assets/spinner.gif'
 const Spinner = () => {
   return (
   
-    <img src={spinner} alt="Loading" style={{width: '100px', margin:'auto', display:'block'}}/>
+    <img src={spinner} alt="Loading" className="Spinner" />
 
 
   )
